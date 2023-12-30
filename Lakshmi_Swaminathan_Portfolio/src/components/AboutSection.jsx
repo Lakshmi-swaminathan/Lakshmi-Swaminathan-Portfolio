994fbaaca6345182b6fc8ac70484b960d9186759
Lakshmi <lakshmiswaminathan08@gmail.com>
2023-12-30 14:35:44 -0500

    About and Home component animations added

--- Lakshmi_Swaminathan_Portfolio/src/components/AboutSection.jsx
+++ Lakshmi_Swaminathan_Portfolio/src/components/AboutSection.jsx
@@ -1,20 +1,77 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const zoomIn = keyframes`
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.05);
+  }
+`;
 
 const AboutContainer = styled.div`
-  height: 100vh;
-  overflow-y: scroll;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 80px 40px;
   background-color: #f9f9f9;
   color: #333;
-  padding: 120px 40px 40px;
-  width: 100vw; /* Take up the entire viewport width */
+  width: 100vw;
+  text-align: left;
+  border: 2px double #3498db;
+  border-radius: 10px;
+  overflow: hidden;
+  transition: transform 0.3s ease;
+  margin: 20px 0; /* Center the component vertically */
+  
+  &:hover {
+    animation: ${zoomIn} 0.3s ease-in-out;
+    transform: scale(1.05);
+  }
+`;
+
+const AboutContent = styled.div`
+  max-width: 800px;
+  border: 1px solid #3498db;
+  padding: 20px;
+  border-radius: 8px;
+`;
+
+const AboutHeading = styled.h2`
+  color: #3498db;
+  margin-bottom: 20px;
+`;
+
+const AboutParagraph = styled.p`
+  line-height: 1.6;
+  margin-bottom: 20px;
 `;
 
 const AboutSection = () => {
   return (
     <AboutContainer id="about">
-      <h2>About Me</h2>
-      <p>"We're here to put a dent in the Universe!!" - Steve Jobs<br/> I'm a passionate coder. Tech enthusiast. A person who loves to learn things at a deeper level. My long term goal is to make people's lives easier leveraging technology.<br/> I love solving problems, building solutions in the most optimal way possible. </p>
+      <AboutContent>
+        <AboutHeading>About Me</AboutHeading>
+        <AboutParagraph>
+          "We're here to put a dent in the Universe!!" - Steve Jobs
+        </AboutParagraph>
+        <AboutParagraph>
+          Hi, I'm Lakshmi Swaminathan, a Full Stack Developer. With a passion for coding and technology, I consider myself a perpetual learner, always striving to delve into the intricacies of programming and beyond. My primary objective is to harness technology to simplify and enhance people's lives.
+        </AboutParagraph>
+        <AboutParagraph>
+          I derive immense satisfaction from problem-solving and take pride in constructing efficient, optimal solutions. I have experience working in various technologies, including Web development, Machine learning, deep learning, and more.
+        </AboutParagraph>
+        <AboutParagraph>
+          Currently pursuing a Master's in Computer Science at Purdue University, I am deeply engrossed in web development, well-versed in MERN stack.
+        </AboutParagraph>
+        <AboutParagraph>
+          Outside the realm of coding, I am an advocate for continuous learning. With a keen eye for detail and a commitment to quality, I have been recognized with Spotlight Awards for Quality/Process improvement, Accountability, and Personal commitment at Verizon.
+        </AboutParagraph>
+        <AboutParagraph>
+          Feel free to explore my portfolio and get in touch. Let's create something extraordinary!
+        </AboutParagraph>
+      </AboutContent>
     </AboutContainer>
   );
 };
--- Lakshmi_Swaminathan_Portfolio/src/components/HomeSection.jsx
+++ Lakshmi_Swaminathan_Portfolio/src/components/HomeSection.jsx
@@ -1,34 +1,49 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const zoomIn = keyframes`
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.05);
+  }
+`;
 
 const HomeContainer = styled.div`
   min-height: 100vh;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color:  rgba(77, 76, 76, 0.87);
-  padding: 0 20px;
-  box-sizing: border-box; /* Include padding and borders in the width calculation */
-  width: 100vw; /* Take up the entire viewport width */
+  background-color: rgba(77, 76, 76, 0.87);
+  padding: 90px 50px 50px; /* Increased padding for top, left, and right */
+  box-sizing: border-box;
+  width: 100vw;
+  transition: transform 0.3s ease;
+
+  &:hover {
+    animation: ${zoomIn} 0.3s ease-in-out;
+    transform: scale(1.05);
+  }
 `;
 
 const HomeImage = styled.img`
   width: 200px;
-  height: 300px; /* Adjust the size as needed */
-  object-fit: cover; /* Maintain aspect ratio */
+  height: 300px;
+  object-fit: cover;
   border-radius: 10px;
   margin-right: 20px;
 `;
 
 const ContentContainer = styled.div`
   text-align: left;
-  color: #fff; /* White text color */
+  color: #fff;
 `;
 
 const Role = styled.h3`
   margin-top: 10px;
   font-weight: bold;
-  color:rgba(134, 195, 245, 0.87);
+  color: rgba(134, 195, 245, 0.87);
 `;
 
 const IconsContainer = styled.div`
@@ -68,27 +83,28 @@ const DownloadButton = styled.a`
     background-color: #2980b9;
   }
 `;
+
 const HomeSection = () => {
-    return (
-      <HomeContainer id="home">
-        <ContentContainer>
-          <HomeImage src="../../public/images/Lakshmi.jpg" alt="Lakshmi Swaminathan" />
-          <h1>Lakshmi Swaminathan</h1>
-          <Role>Full Stack Developer <br/> Master's in Computer Science, Purdue University <br/> Ex-SDE-II at Verizon<br/></Role>
-          <DownloadButton href="../../public/Lakshmi_Swaminathan_Resume.pdf" download>
-            Download Resume
-          </DownloadButton>
-        </ContentContainer>
-        <IconsContainer>
-          <IconLink href="https://github.com/Lakshmi-swaminathan" target="_blank" rel="noopener noreferrer">
-          <IconImage src=".../../public/images/Github_icon.png" alt="GitHub" />
-          </IconLink>
-          <IconLink href="https://www.linkedin.com/in/lakshmi-swaminathan-g/" target="_blank" rel="noopener noreferrer">
+  return (
+    <HomeContainer id="home">
+      <ContentContainer>
+        <HomeImage src="../../public/images/Lakshmi.jpg" alt="Lakshmi Swaminathan" />
+        <h1>Lakshmi Swaminathan</h1>
+        <Role>Full Stack Developer <br /> Master's in Computer Science, Purdue University <br /> Ex-SDE-II at Verizon<br /></Role>
+        <DownloadButton href="../../public/Lakshmi_Swaminathan_Resume.pdf" download>
+          Download Resume
+        </DownloadButton>
+      </ContentContainer>
+      <IconsContainer>
+        <IconLink href="https://github.com/Lakshmi-swaminathan" target="_blank" rel="noopener noreferrer">
+          <IconImage src="../../public/images/Github_icon.png" alt="GitHub" />
+        </IconLink>
+        <IconLink href="https://www.linkedin.com/in/lakshmi-swaminathan-g/" target="_blank" rel="noopener noreferrer">
           <IconImage src="../../public/images/LinkedIn.png" alt="LinkedIn" />
-          </IconLink>
-        </IconsContainer>
-      </HomeContainer>
-    );
-  };
+        </IconLink>
+      </IconsContainer>
+    </HomeContainer>
+  );
+};
 
 export default HomeSection;
